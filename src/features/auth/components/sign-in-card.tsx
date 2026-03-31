5574970bada3f86740ed51a7a07fe5da2d768ee6
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-03-31 13:48:54 +1200

    standard form added to signin component

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,10 +1,22 @@
+import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card"
 
 
 export const SignInCard = () => {
   return (
-    <div>
-        Sign in 
-    </div>
+    <Card className="w-full h-full p-8">
+      <CardHeader className="px-0 pt-0">
+        Card header
+      </CardHeader>
+      <CardDescription>
+        Use your email or another service to continue
+      </CardDescription>
+      <CardContent className="space-y-5 px-0 pb-0">
+        <form className="space-y-5 ">
+
+        </form>
+      </CardContent>
+      Sign in
+    </Card>
   )
 }
 
