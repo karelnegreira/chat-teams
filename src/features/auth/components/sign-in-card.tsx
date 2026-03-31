802c5a62def1cb61d641071547cc158b6947aa38
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-03-31 14:02:27 +1200

    added some style to sign in form

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,18 +1,40 @@
-import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
 
 
 export const SignInCard = () => {
   return (
     <Card className="w-full h-full p-8">
       <CardHeader className="px-0 pt-0">
-        Card header
+        <CardTitle>
+          Log in to continue
+        </CardTitle>
+        <CardDescription>
+          Use your email or another service to continue
+        </CardDescription>
       </CardHeader>
-      <CardDescription>
-        Use your email or another service to continue
-      </CardDescription>
       <CardContent className="space-y-5 px-0 pb-0">
-        <form className="space-y-5 ">
-
+        <form className="space-y-2.5 ">
+          <Input
+            disabled={false}
+            value=""
+            onChange={() => {}}
+            placeholder="Email"
+            type="email"
+            required
+          />
+          <Input
+            disabled={false}
+            value=""
+            onChange={() => {}}
+            placeholder="Password"
+            type="password"
+            required
+          />
+          <Button type="submit" className="w-full" size="lg" disabled={false}>
+            Continue
+          </Button>
         </form>
       </CardContent>
       Sign in
