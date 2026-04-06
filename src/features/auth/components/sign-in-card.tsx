13da81abd3e9f1e47766eade3caa15c3ff270a9e
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-04-06 13:01:44 +1200

    adding some navigation btw sign in and sign up thru setstate props

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -6,9 +6,14 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
+import { SignInFlow } from '../types';
+
+interface SignInCardProps {
+  setState: (state: SignInFlow) => void, 
+}
 
 
-export const SignInCard = () => {
+export const SignInCard = ({ setState }: SignInCardProps) => {
   return (
     <Card className="w-full h-full p-8">
       <CardHeader className="px-0 pt-0">
@@ -50,7 +55,7 @@ export const SignInCard = () => {
               size="lg"
               className="w-full relative bg-white text-black"
             >
-              <FcGoogle  className="size-5 absolute top-2.5 left-2.5"/>
+              <FcGoogle  className="size-5 absolute top-3 left-2.5"/>
               Continue with Google
             </Button>
 
@@ -61,10 +66,13 @@ export const SignInCard = () => {
               size="lg"
               className="w-full relative bg-white text-black"
             >
-              <FaGithub  className="size-5 absolute top-2.5 left-2.5"/>
+              <FaGithub  className="size-5 absolute top-3 left-2.5"/>
               Continue with Github
             </Button>
           </div>
+          <p className='text-xs text-muted-foreground justify-center'>
+            Don't have an account?<span onClick={() => setState("signUp")} className='text-sky-700 hover:underline cursor-pointer'>Sign up</span>
+          </p>
       </CardContent>
     </Card>
   )
