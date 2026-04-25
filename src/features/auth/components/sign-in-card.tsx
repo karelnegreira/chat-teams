f674e986c4443814dc83cb3228d8ed0670ce7f1c
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-04-25 16:10:08 +1200

    onPasswordSignIn function is coded and made some verification before flagging an error

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
+import { TriangleAlert } from 'lucide-react';
 import { SignInFlow } from '../types';
 import React, { useState } from 'react';
 import { useAuthActions } from '@convex-dev/auth/react';
@@ -29,7 +30,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
     setPending(true);
     signIn("password", {email, password, flow: "signIn" }) 
       .catch(() => {
-
+        setError("Invalid email or password")
       })
       .finally(() => {
         setPending(false);
@@ -54,8 +55,14 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
           Use your email or another service to continue
         </CardDescription>
       </CardHeader>
+        {!!error && (
+          <div className='bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6'>
+            <TriangleAlert className="size-4" />
+            <p>{error}</p>
+          </div>
+        ) }
       <CardContent className="space-y-5 px-0 pb-0">
-        <form className="space-y-2.5 ">
+        <form onSubmit={onPasswordSignIn} className="space-y-2.5 ">
           <Input
             disabled={pending}
             value={email}
