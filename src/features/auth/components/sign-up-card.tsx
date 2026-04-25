5294e5225c1702500dee96a54e5c32270a6d34b0
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-04-25 16:53:58 +1200

    passowrd sign up and login options are fully functional

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -5,9 +5,12 @@ import { FaGithub } from 'react-icons/fa';
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
+import {TriangleAlert} from 'lucide-react';
+
 import { Separator } from "@/components/ui/separator"
 import { SignInFlow } from '../types';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { useAuthActions } from '@convex-dev/auth/react';
 
 interface SignUpCardProps {
   setState: (state: SignInFlow) => void, 
@@ -16,10 +19,39 @@ interface SignUpCardProps {
 
 export const SignUpCard = ({ setState }: SignUpCardProps) => {
 
+  const { signIn } = useAuthActions();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const [pending, setPending] = useState(false);
 
+  const onPasswordSignUp = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setError("Passowrds or email doesn't match")
+      return;
+    }
+
+    setPending(true);
+    signIn("password", {email, password, flow: "signUp"})
+      .catch(() => {
+        setError("Something went wrong")
+      })
+      .finally(() => {
+        setPending(false)
+      })
+  }
+
+  const handleProviderSignUp = (value: "github" | "google") => {
+    setPending(true);
+    signIn(value)
+      .finally(() => {
+        setPending(false);
+      })
+  }
 
   return (
     <Card className="w-full h-full p-8">
@@ -31,8 +63,16 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
           Use your email or another service to continue
         </CardDescription>
       </CardHeader>
+
+      {!!error && (
+          <div className='bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6'>
+            <TriangleAlert className="size-4" />
+            <p>{error}</p>
+          </div>
+        ) }
+
       <CardContent className="space-y-5 px-0 pb-0">
-        <form className="space-y-2.5 ">
+        <form onSubmit={onPasswordSignUp} className="space-y-2.5 ">
           <Input
             disabled={false}
             value={email}
@@ -57,15 +97,15 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
             type="password"
             required
           />
-          <Button type="submit" className=" bg-black w-full text-cyan-50" size="lg" disabled={false}>
+          <Button type="submit" className=" bg-black w-full text-cyan-50" size="lg" disabled={pending}>
             Continue
           </Button>
         </form>
         <Separator/>
           <div className="flex flex-col gap-y-2.5">
             <Button
-              disabled={false}
-              onClick={() => {}}
+              disabled={pending}
+              onClick={() => handleProviderSignUp("google")}
               variant="outline"
               size="lg"
               className="w-full relative bg-white text-black"
@@ -75,8 +115,8 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
             </Button>
 
             <Button
-              disabled={false}
-              onClick={() => {}}
+              disabled={pending}
+              onClick={() => handleProviderSignUp("github")}
               variant="outline"
               size="lg"
               className="w-full relative bg-white text-black"
