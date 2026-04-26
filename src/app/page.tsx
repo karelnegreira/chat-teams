bed5143a107c661e258e81bae8db3e2abeb4b0ea
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-04-27 00:36:05 +1200

    removed some innecessary code from app/page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
 "use client";
 
 import { UserButton } from '@/features/auth/components/user-button';
-import { useAuthActions } from '@convex-dev/auth/react';
-import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  const { signOut } = useAuthActions();
-  const  router  = useRouter()
-
-  const handleSignOut = async () => {
-    await signOut();
-    location.replace("/auth");
-  };
   
   return (
     <div>
-      Logged in
       <UserButton />
     </div>
   );
