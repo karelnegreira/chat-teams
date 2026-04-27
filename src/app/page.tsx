51e2368eaaa89e79013d6ac35c55ef04c26db6c7
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-04-28 04:26:02 +1200

    use-create-workspace-modal hook created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
 "use client";
 
 import { UserButton } from '@/features/auth/components/user-button';
+import { useGetWorkspaces } from '@/features/workspaces/api/user-get-workspace';
+import { useEffect, useMemo } from 'react';
 
 export default function Home() {
+
+  const {data, isLoading} = useGetWorkspaces();
+
+  const workspaceId = useMemo(() => data?.[0]._id, [data]);
+
+  useEffect(() => {
+    if (isLoading) return;
+    if (workspaceId) {
+      console.log("Redirect to workspace")
+    } else {
+      console.log("Open creation modal")
+    }
+  }, [workspaceId, isLoading])
   
   return (
     <div>
