edf287c53becc7d5d523f636258f0f8afb43c2e0
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-04-29 02:40:58 +1200

    method create was coded into workspaces.ts into the convex folder to fetch data

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,4 +1,28 @@
-import { query } from './_generated/server';
+import { auth } from './auth';
+import { mutation, query } from './_generated/server';
+
+export const create = mutation({
+    args: {
+
+    }, 
+    handler: async (ctx, args) => {
+        const userId = await auth.getUserId(ctx);
+
+        if (!userId) {
+            throw new Error("Unauthorized")
+        }
+        //TODO: Create a proper method
+        const joinCode = "12345";
+
+        const workspaceId = await ctx.db.insert("workspaces", {
+            name: args.name, 
+            userId, 
+            joinCode,
+        });
+
+        return workspaceId;
+    }, 
+})
 
 export const get = query({
     args: {}, 
