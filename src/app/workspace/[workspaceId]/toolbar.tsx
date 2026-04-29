5401169e04166315c66d5397477602b8f880cdaa
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-04-29 23:29:55 +1200

    search icon visible at toolbar

---
+++ src/app/workspace/[workspaceId]/toolbar.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+import { Search } from 'lucide-react';
+
+export const Toolbar = () => {
+    return (
+        <nav className="bg-[#481349] flex items-center justify-between h-10 p-1.5">
+            <div className="flex-1" />
+            <div className="min-w-[280px] max-[642px] grow-[2] shrink">
+                <Button size='sm' className="bg-accent/25 hover:bg-accent-25 w-full justify-start h-7 px-2">
+                    <Search className="size-4 text-white mr-2"/>
+                </Button>
+            </div>
+        </nav>
+    )
+}
