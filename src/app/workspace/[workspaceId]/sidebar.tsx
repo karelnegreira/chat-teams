246a54688063f718fbe2bebde94728e00eb7d727
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-04-30 22:00:43 +1200

    sidebar component now being coded

---
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -0,0 +1,9 @@
+
+
+export const Sidebar = () => {
+    return (
+        <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center">
+            Sidebar
+        </aside>
+    )
+}
