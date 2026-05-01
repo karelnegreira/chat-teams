3b4d75e539e7227858c1cf79ee376e87917a81b2
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-05-01 20:15:17 +1200

    Sidebar button activity, DMs, More, Home are now visible

--- src/app/workspace/[workspaceId]/sidebar-button.tsx
+++ src/app/workspace/[workspaceId]/sidebar-button.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
 import { LucideIcon } from 'lucide-react';
 import { LucideType } from 'react-icons/lib';
 
@@ -8,12 +9,19 @@ interface SidebarButtonProps {
     isActive?: boolean;
 }
 
-export const SidebarButton = ({icon, label, isActive} : SidebarButtonProps) => {
+export const SidebarButton = ({icon: Icon, label, isActive} : SidebarButtonProps) => {
     return (
         <div className="flex flex-col items-center justify-center gap-y-0.5 cursor-pointer group">
-            <Button className="bg-black">
-
+            <Button 
+                variant="transparent" 
+                className={cn(`size-9 p-2 group-hover:bg-accent/20`, 
+                            isActive && "bg-accent/20"
+                            )}>
+                    <Icon className="size-6 text-white group-hover:scale-110 transition-all" />
             </Button>
+            <span className="text-[11px] text-white group-hover:text-accent">
+                {label}
+            </span>
         </div>
     )
 }
--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -1,4 +1,4 @@
-import {Home} from 'lucide-react';
+import {Home, MessageSquare, Bell, MoreHorizontal} from 'lucide-react';
 
 import { UserButton } from "@/features/auth/components/user-button"
 import { SidebarButton } from "./sidebar-button"
@@ -10,6 +10,9 @@ export const Sidebar = () => {
         <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-[9px] pb-4">
             <WorkspaceSwitcher />
             <SidebarButton icon={Home} label="Home" isActive />
+            <SidebarButton icon={MessageSquare} label="DMs" />
+            <SidebarButton icon={Bell} label="Activity" />
+            <SidebarButton icon={MoreHorizontal} label="More" />
             <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
                 <UserButton />
             </div>
