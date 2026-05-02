8cc75b95b33316ec10f325ab25ab2b212cee3fd9
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-05-03 10:42:03 +1200

    button reflecting workspace name is now visible

--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -1,9 +1,33 @@
 
+import { Button } from '@/components/ui/button'
+import {
+    DropdownMenu, 
+    DropdownMenuContent, 
+    DropdownMenuItem, 
+    DropdownMenuSeparator, 
+    DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { Doc } from '../../../../convex/_generated/dataModel'
 
-export const WorkspaceHeader = () => {
+interface WorkspaceHeaderProps {
+    workspace: Doc<"workspaces">, 
+}
+
+
+export const WorkspaceHeader = ({workspace}: WorkspaceHeaderProps) => {
     return (
-        <div>
-            
+        <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button
+                        variant="transparent"
+                        className="font-semibold text-lg w-auto p-1.5 overflow-hidden"
+                        size="sm"
+                    >
+                        <span>{workspace?.name}</span>
+                    </Button>
+                </DropdownMenuTrigger>
+            </DropdownMenu>
         </div>
     )
 }
