03cbc3d5dc557712a5cb96ad8b952a3f61515553
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-05-04 04:58:33 +1200

    dorpdown menu content is now displaying workspace info

--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -21,7 +21,7 @@ export const WorkspaceHeader = ({workspace}: WorkspaceHeaderProps) => {
     return (
         <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
             <DropdownMenu>
-                <DropdownMenuTrigger asChild>
+                <DropdownMenuTrigger>
                     <Button
                         variant="transparent"
                         className="font-semibold text-lg w-auto p-1.5 overflow-hidden"
@@ -31,12 +31,20 @@ export const WorkspaceHeader = ({workspace}: WorkspaceHeaderProps) => {
                         <ChevronDown className="size-4 ml-1 shrink-0" />
                     </Button>
                 </DropdownMenuTrigger>
-                <DropdownMenuContent side="bottom" align="start" className="w-64">
+                <DropdownMenuContent
+                    side="bottom"
+                    align="start"
+                    className="w-64 bg-gray-800 text-white border border-gray-700 shadow-md rounded-md"
+                >
                     <DropdownMenuItem
                         className="cursor-pointer capitalize"
                     >
-                        <div className="size-9 relative overflow-hidden bg-[#616061] text-white font-semibold text-xl rounded-md flex items-center justify-center">
+                        <div className="size-9 relative overflow-hidden bg-[#616061] text-white font-semibold text-xl rounded-md flex items-center justify-center mr-2">
                             {workspace.name.charAt(0).toUpperCase()}
+                        </div>
+                        <div className="flex flex-col items-start">
+                            <p className="font-bold">{workspace.name}</p>
+                            <p className="text-xs text-muted-foreground">Active workspace</p>
                         </div>
                     </DropdownMenuItem>
                 </DropdownMenuContent>
