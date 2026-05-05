86d4cc6a0ab41f1c8477f61e30b5cf60ea09363c
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-05-06 05:34:36 +1200

    preferences modal component coded yet under develop

--- src/app/workspace/[workspaceId]/preferences-modal.tsx
+++ src/app/workspace/[workspaceId]/preferences-modal.tsx
@@ -1,9 +1,61 @@
+import {useState} from 'react';
+import {TrashIcon} from 'lucide-react';
+
+import {
+    Dialog,
+    DialogPortal,
+    DialogOverlay,
+    DialogClose,
+    DialogTrigger,
+    DialogContent,
+    DialogHeader,
+    DialogFooter,
+    DialogTitle,
+    DialogDescription,
+  } from '@/components/ui/dialog'; 
+import { Button } from '@/components/ui/button';
+
+  interface PreferencesModalProps {
+    open:  boolean;
+    setOpen: (open: boolean) => void;
+    initialValue: string;
+  }
 
 
-export const PreferencesModal = () => {
+export const PreferencesModal = ({open, setOpen, initialValue}: PreferencesModalProps) => {
+    const [value, setValue] = useState(initialValue)
     return (
-        <div>
-            Preferences modal
-        </div>
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogContent className="p-0 bg-gray-50 overflow-hidden">
+                <DialogHeader className="p-4 border-b bg-white">
+                    <DialogTitle>
+                        {value}
+                    </DialogTitle>
+                </DialogHeader>
+                <div className='px-4 pb-4 flex flex-col gap-y-2'>
+                    <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
+                        <div className='flex items-center justify-between'>
+                            <p className="text-sm font-semibold">
+                                Workspace name
+                            </p>
+                            <p className="text-sm text-[#1264a3] hover:underline font-semibold">
+                                Edit
+                            </p>
+                        </div>
+                        <p className="text-sm">
+                             {value}
+                        </p>
+                    </div>
+                </div>
+                <Button
+                    disabled={false}
+                    onClick={() => {}}
+                    className="flex items-center justify-start gap-x-2 px-5 py-4 bg-white rounded-lg  border cursor-pointer hover:bg-gray-50 text-rose-600"
+                >
+                    <TrashIcon className="size-4"/>
+                    <p className="text-sm font-semibold">Delete this workspace</p>
+                </Button>
+            </DialogContent>
+        </Dialog>
     )
 }
