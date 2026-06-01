53db912e1c81d342846eed03fae19c7872cc7395
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-06-01 15:53:56 +1200

    sidebarItemVariants function coded and added to sidebaritems props

--- src/app/workspace/[workspaceId]/sidebar-items.tsx
+++ src/app/workspace/[workspaceId]/sidebar-items.tsx
@@ -3,20 +3,36 @@ import { Button } from '@/components/ui/button';
 import {LucideIcon} from 'lucide-react';
 import {IconType} from 'react-icons/lib';
 import Link from "next/link";
+import {cva, type VariantProps} from 'class-variance-authority';
 import { useWorkspaceId } from '@/hooks/use-workspace-id';
+
+const sidebarItemVariants = cva(
+    "flex items-center gap-1.5 justify-center font-normal h-7 px-[18px] text-sm overflow-hidden", 
+    {
+        variants: {
+            variant: {
+                default: "text-[#f9edffcc]", 
+                active: "text-[#481349] bg-white/90 hover:bg-white/90"
+            }, 
+        }, 
+    }, 
+);
 
 interface SidebarItemsProps {
     label: string; 
     id: string;
     icon: LucideIcon | IconType;
+    variant?: VariantProps<typeof sidebarItemVariants>["variant"]
 };
+
+
 
 export const SidebarItems = ({label, id, icon: Icon}: SidebarItemsProps) => {
     const workspaceId = useWorkspaceId();
     return (
         <Button
             variant="transparent"
-            size="sm"
+            size="lg"
             asChild
         >
             <Link href={`/workspace/${workspaceId}/channel/${id}`}>
