f1a899f50f47ca1690c9589ec941d959d439c8cd
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-06-18 21:24:43 +1200

    adding style to FaCaretDown icon

--- src/app/workspace/[workspaceId]/workspace-section.tsx
+++ src/app/workspace/[workspaceId]/workspace-section.tsx
@@ -1,4 +1,7 @@
 
+import { FaCaretDown } from 'react-icons/fa';
+import { Button } from "@/components/ui/button";
+
 interface WorkspaceSectonProps {
     children: React.ReactNode; 
     label: string;
@@ -8,7 +11,15 @@ interface WorkspaceSectonProps {
 
 export const WorkspaceSection = ({children, label, hint, onNew}: WorkspaceSectonProps) => {
     return (
-        <div>
+        <div className="flex flex-col mt-3 px-2">
+            <div className="flex items-center px-3.5 group">
+                <Button
+                    variant="transparent"
+                    className="p-0.5 text-sm text-[#f9edffcc] shink-0 size-6"
+                >
+                    <FaCaretDown className="size-4"/>
+                </Button>
+            </div>
             {children}
         </div>
     )
