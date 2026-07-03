b968c9260cb633efde5b99c928be8b186888c46f
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-07-04 06:31:27 +1200

    button disabled parameter set to isPending at create channel modal

--- src/features/channels/components/create-channel-modal.tsx
+++ src/features/channels/components/create-channel-modal.tsx
@@ -63,7 +63,7 @@ export const CreateChannelModal = () => {
 
                         />
                         <div className="flex justify-end">
-                            <Button disabled={false}>
+                            <Button disabled={isPending}>
                                 Create
                             </Button>   
                         </div>
