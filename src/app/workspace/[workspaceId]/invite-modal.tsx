ce6fa5d03c04227a96d198d3abe8a93fc0e5575c
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-07-11 04:44:34 +1200

    invite copy link function coded and running

--- src/app/workspace/[workspaceId]/invite-modal.tsx
+++ src/app/workspace/[workspaceId]/invite-modal.tsx
@@ -1,4 +1,6 @@
-
+import {CopyIcon} from 'lucide-react';
+import {toast} from 'sonner';
+import { Button } from '@/components/ui/button';
 import {
     Dialog, 
     DialogClose, 
@@ -7,6 +9,7 @@ import {
     DialogHeader, 
     DialogTitle
 } from '@/components/ui/dialog';
+import { useWorkspaceId } from '@/hooks/use-workspace-id';
 
 interface InviteModalProps {
     open: boolean;
@@ -15,7 +18,20 @@ interface InviteModalProps {
     joinCode: string;
 }
 
+
+
 export const InviteModal = ({open, setOpen, name, joinCode}: InviteModalProps) => {
+
+    const workspaceId = useWorkspaceId();
+
+    const handleCopy = () => {
+        const invitelink = `${window.location.origin}/join/${workspaceId}`;
+
+        window.navigator.clipboard
+        .writeText(invitelink)
+        .then(() => toast.success("Invite link copied to clipboard"));
+    }
+
     return (
         <Dialog  open={open} onOpenChange={setOpen}>
             <DialogContent className="bg-white">
@@ -25,6 +41,19 @@ export const InviteModal = ({open, setOpen, name, joinCode}: InviteModalProps) =
                         Use the code below to invite people to your workspace 
                     </DialogDescription>
                 </DialogHeader>
+                <div className='flex flex-col gap-y-4 items-center justify-center py-10'>
+                    <p className="text-4xl font-bold tracking-widest uppercase">
+                        {joinCode}
+                    </p>
+                    <Button
+                        onClick={handleCopy}
+                        variant='ghost'
+                        size='sm'
+                    >
+                        Copy link
+                        <CopyIcon className="size-4 ml-2"/>
+                    </Button>
+                </div>
             </DialogContent>
         </Dialog>
     )
