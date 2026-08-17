a57de25947c079d2a583fac4654be8149e88e33f
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-08-18 11:13:50 +1200

    getById function was coded in channels convex backend

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -33,7 +33,7 @@ const WorkspaceIdPage = () => {
 
     }, [channelId, workspaceLoading, channelsLoading, workspace, open, router, workspaceId, member, memberLoading, isAdmin])
 
-    if (workspaceLoading || channelsLoading) {
+    if (workspaceLoading || channelsLoading || memberLoading) {
         return (
             <div className='h-full flex-1 flex items-center justify-center flex-col gap-2'>
                 <Loader className="size-6 animate-spin text-muted-foreground"  />
@@ -41,7 +41,7 @@ const WorkspaceIdPage = () => {
         )
     }
 
-    if (!workspace) {
+    if (!workspace || !member) {
         return (
             <div className='h-full flex-1 flex items-center justify-center flex-col gap-2'>
                 <TriangleAlert className="size-6 text-muted-foreground"  />
