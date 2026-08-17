318ecfe25dca9e3be488ec871d55c318aecb5ab4
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-08-18 02:21:27 +1200

    isadmin conditional was added in order to create channels

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -8,29 +8,30 @@ import { useCreateChannelModal } from "@/features/channels/store/use-create-chan
 import { useGetWorkspace } from "@/features/workspaces/api/user-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { useRouter } from "../../../../node_modules/next/navigation";
+import { useCurrentMember } from '@/features/members/api/use-current-member';
 
 const WorkspaceIdPage = () => {
     const workspaceId = useWorkspaceId();
     const router = useRouter();
     const [open, setOpen] = useCreateChannelModal()
 
+    const {data: member, isLoading: memberLoading} = useCurrentMember({workspaceId});
     const {data: workspace, isLoading: workspaceLoading} = useGetWorkspace({id: workspaceId});
     const {data: channels, isLoading: channelsLoading} = useGetChannels({workspaceId});
 
-    const channelId = useMemo(() => 
-        channels?.[0]?._id
-    , [channels])
+    const channelId = useMemo(() => channels?.[0]?._id, [channels])
+    const isAdmin = useMemo(() => member?.role === 'admin', [member?.role])
 
     useEffect(() => {
-        if (workspaceLoading || channelsLoading || !workspace) return;
+        if (workspaceLoading || channelsLoading || memberLoading || !member || !workspace) return;
 
         if (channelId) {
             router.push(`/workspace/${workspaceId}/channel/${channelId}`)
-        } else if (!open) {
+        } else if (!open && isAdmin) {
             setOpen(true)
         } 
 
-    }, [channelId, workspaceLoading, channelsLoading, workspace, open, router, workspaceId])
+    }, [channelId, workspaceLoading, channelsLoading, workspace, open, router, workspaceId, member, memberLoading, isAdmin])
 
     if (workspaceLoading || channelsLoading) {
         return (
@@ -43,7 +44,7 @@ const WorkspaceIdPage = () => {
     if (!workspace) {
         return (
             <div className='h-full flex-1 flex items-center justify-center flex-col gap-2'>
-                <TriangleAlert className="size-6 animate-spin text-muted-foreground"  />
+                <TriangleAlert className="size-6 text-muted-foreground"  />
                 <span className="text-sm text-muted-foreground">
                     Workspace not found
                 </span>
@@ -51,7 +52,14 @@ const WorkspaceIdPage = () => {
         );
     }
 
-    return null;
+    return  (
+        <div className='h-full flex-1 flex items-center justify-center flex-col gap-2'>
+            <TriangleAlert className="size-6 text-muted-foreground"  />
+            <span className="text-sm text-muted-foreground">
+                No channel found
+            </span>
+        </div>
+    );;
 };
 
 export default WorkspaceIdPage;
