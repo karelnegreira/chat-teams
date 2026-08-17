5938baadfc2030aadedfa92fb1abcaf5b7095589
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-08-18 01:40:39 +1200

    channel that is pointed gets highlighted

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -11,15 +11,20 @@ import { useGetMembers } from '@/features/members/api/use-get-members';
 import { UserItem } from './user-item';
 import { useCreateChannelModal } from '@/features/channels/store/use-create-channel-modal';
 
+import { useChannelId } from '@/hooks/use-channel-id';
 
 export const WorkspaceSidebar = () => {
+    const channelId = useChannelId()
     const workspaceId = useWorkspaceId();
     const [_open, setOpen] = useCreateChannelModal();
+   
 
     const {data: member, isLoading: memberLoading} = useCurrentMember({workspaceId});
     const {data: workspace, isLoading: workspaceLoading} = useGetWorkspace({id: workspaceId});
     const {data: channels, isLoading: channelsLoading} = useGetChannels({workspaceId})
     const {data: members, isLoading: membersLoading} = useGetMembers({ workspaceId })
+
+
 
     if (workspaceLoading || memberLoading) {
         return (
@@ -66,6 +71,7 @@ export const WorkspaceSidebar = () => {
                         icon={HashIcon}
                         label={item.name}
                         id={item._id}
+                        variant={channelId === item._id ? "active" : "default"}
                     />
                 ))}
             </WorkspaceSection>
