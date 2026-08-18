78b0bf3c74d9c815c9098e81417f7e3a19a6ee8d
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-08-18 12:33:08 +1200

    channel header dialogs are displayable

--- src/app/workspace/[workspaceId]/channel/[channelId]/header.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/header.tsx
@@ -2,6 +2,7 @@
 import {FaChevronDown} from 'react-icons/fa';
 
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 
 interface HeaderProps {
     name: string;
@@ -10,14 +11,25 @@ interface HeaderProps {
 export  const Header = ({ name }: HeaderProps) => {
   return (
     <div className="bg-white border-b h-[49px] flex items-center px-4 overflow-hidden">
-      <Button
-        variant="ghost"
-        className="text-lg font-semibold px-2 overflow-hidden w-auto"
-        size="sm"
-      >
-        <span className="truncate"># {name}</span>
-        <FaChevronDown className="size-2.5 ml-2" />
-      </Button>
+      <Dialog>
+        <DialogTrigger asChild>
+            <Button
+                variant="ghost"
+                className="text-lg font-semibold px-2 overflow-hidden w-auto"
+                size="sm"
+            >
+                <span className="truncate"># {name}</span>
+                <FaChevronDown className="size-2.5 ml-2" />
+            </Button>
+        </DialogTrigger>
+        <DialogContent className="p-0 bg-gray-50 overflow-hidden">
+            <DialogHeader className="p-4 border-b bg-white">
+                <DialogTitle>
+                    # {name}
+                </DialogTitle>
+            </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   )
 }
