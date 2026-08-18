b96306d019adf2901015b251a46f82c30e462165
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-08-18 14:27:38 +1200

    remove function added to channels backend

--- convex/channels.ts
+++ convex/channels.ts
@@ -41,6 +41,44 @@ export const update = mutation({
 })
 
 
+
+export const remove = mutation({
+    args: {id: v.id("channels"), 
+    }, 
+    handler: async (ctx, args) => {
+        const userId = await auth.getUserId(ctx);
+
+        if (!userId) {
+            throw new Error("Unauthorized")
+        }
+
+        const channel = ctx.db.get(args.id)
+
+        if (!channel) {
+            throw new Error("Channel not found")
+        }
+
+        const member = await ctx.db
+                .query("members")
+                .withIndex("by_workspace_id_user_id", 
+                (q) => q.eq("workspaceId", channel.workspaceId)
+                .eq("userId", userId), 
+                )
+                .unique();
+
+        if (!member || member.role !== "admin") {
+            throw new Error("Unauthorized")
+        }
+
+        //TODO: remove associated messages
+
+        await ctx.db.delete(args.id)
+
+        return args.id;
+    },
+})
+
+
 export const create = mutation({
     args: {
         name: v.string(), 
