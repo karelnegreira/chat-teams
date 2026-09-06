5a4c0fcd31d30b9b08e62efdb3944a1a72faec61
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-09-07 04:17:26 +1200

    quill package added

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -2,8 +2,10 @@
 
 const Editor = () => {
     return (
-        <div>
-            
+        <div className="flex flex-col">
+            <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
+                <div>Editor</div>
+            </div>
         </div>
     )
 }
