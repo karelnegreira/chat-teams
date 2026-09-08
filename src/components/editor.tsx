1fd5a1b1817787280ee32c95ed9b76870511c532
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-09-08 13:17:08 +1200

    styles added to globals.css

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -30,7 +30,7 @@ const Editor = () => {
     return (
         <div className="flex flex-col">
             <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
-                <div ref={containerRef}/>
+                <div ref={containerRef} className="h-full ql-custom"/>
             </div>
         </div>
     )
