e08f6b940309dc25442658fd32eb36e6f3da35b9
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-09-08 15:30:40 +1200

    variants added to hide and show functions depend on if editor is in create or update variant

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -9,7 +9,11 @@ import { Button } from './ui/button';
 import { Hint } from './hint';
 import "quill/dist/quill.snow.css";
 
-const Editor = () => {
+interface EditorProps {
+    variant?: "create" | "update"
+};
+
+const Editor = ( { variant= "create" }: EditorProps) => {
     const containerRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
@@ -38,6 +42,7 @@ const Editor = () => {
             <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
                 <div ref={containerRef} className="h-full ql-custom"/>
                 <div className="flex px-2 pb-2 z-[5]">
+                    
                     <Hint label="Hide formatting">
                         <Button
                             disabled={false}
@@ -48,6 +53,7 @@ const Editor = () => {
                             <PiTextAa className="size-4"/>
                         </Button>
                     </Hint>
+
                     <Hint label="Emoji">
                         <Button
                             disabled={false}
@@ -58,23 +64,48 @@ const Editor = () => {
                             <Smile className="size-4"/>
                         </Button>
                     </Hint>
-                    <Hint label="Attachments">
-                        <Button
+                    {variant==="create" && (
+                        <Hint label="Attachments">
+                            <Button
+                                disabled={false}
+                                size="iconSm"
+                                variant='ghost'
+                                onClick={() => {}}
+                            >
+                                <ImageIcon className="size-4"/>
+                            </Button>
+                        </Hint>
+                    )}
+                    {variant==="update" && (
+                        <div className="ml-auto flex items-center gap-x-2">
+                            <Button
+                                variant='outline'
+                                size="sm"
+                                onClick={() => {}}
+                                disabled={false}
+                            >
+                                Cancel
+                            </Button>
+                            <Button
+                                disabled={false}
+                                onClick={() => {}}
+                                size="sm"
+                                className=" bg-[#007a5a] hover:bg-[#007a5a]/80 text-white"
+                            >
+                                Save
+                            </Button>
+                        </div>
+                    )}
+                    {variant==="create" && (
+                        <Button 
                             disabled={false}
-                            size="iconSm"
-                            variant='ghost'
                             onClick={() => {}}
-                        >
-                            <ImageIcon className="size-4"/>
+                            size='iconSm'
+                            className="ml-auto bg-[#007a5a] hover:bg-[#007a5a]/80 text-white">
+                            <MdSend className="size-4" />
                         </Button>
-                    </Hint>
-                    <Button 
-                        disabled={false}
-                        onClick={() => {}}
-                        size='iconSm'
-                        className="ml-auto bg-[#007a5a] hover:bg-[#007a5a]/80 text-white">
-                        <MdSend className="size-4" />
-                    </Button>
+                    )}
+                    
                 </div>
             </div>
             <div className="p-2 text-[10px] text-muted-foreground flex justify-end">
